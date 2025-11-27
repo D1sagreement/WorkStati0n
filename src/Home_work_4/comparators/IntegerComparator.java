4975b31851f36ba1f234f6320125d67f1c20745e
D1sagreement <hvrti18@gmail.com>
2025-11-27 03:16:42 +0300

    Add files via upload

---
+++ src/Home_work_4/comparators/IntegerComparator.java
@@ -0,0 +1,9 @@
+package Home_work_4.comparators;
+import java.util.Comparator;
+
+public class IntegerComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer i1, Integer i2) {
+        return Integer.compare(i1, i2);
+    }
+}
